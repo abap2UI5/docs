68e379f6484374bbb44a898a1cd3c75dc7965b3e
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2024-11-15 16:29:56 +0000

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -105,7 +105,7 @@ export default defineConfig({
             { text: 'Barcode Scanning', link: '/development/barcodes' },
             { text: 'File Handling', link: '/development/files', },
             { text: 'XLSX', link: '/development/xlsx', },
-            { text: 'Native Device Capabilities', link: '/development/ndc' },
+            { text: 'NDCs', link: '/development/ndc' },
            ]
           },
         ]
