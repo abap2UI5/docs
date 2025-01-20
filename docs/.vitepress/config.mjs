85d80340deb653cd82d69c51bcfe0350f2855056
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2025-01-20 12:56:09 +0100

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -100,8 +100,7 @@ export default defineConfig({
             { text: 'Tables, Trees', link: '/development/model/tables' },
             { text: 'OData', link: '/development/model/odata' },
             { text: 'Device Model', link: '/development/model/device' },
-            { text: 'Expression Binding', link: '/development/model/expression_binding' },
-            { text: 'Fuzzy Search', link: '/development/model/fuzzy' },
+            { text: 'Expression Binding', link: '/development/model/expression_binding' }
           ]},
           { text: 'Events', link: '/development/events' },     
           { text: 'Navigation', link: '/development/navigation/navigation' , collapsed: true , items:[
