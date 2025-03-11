46b15f624e91e746250b9c19974dfc5d78dacee8
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2025-03-11 16:02:50 +0000

    update local version

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -145,6 +145,7 @@ export default defineConfig({
           { text: 'Downporting', link: '/advanced/downporting' },
           { text: 'Renaming', link: '/advanced/renaming' },
           { text: 'Builder', link: '/advanced/builds' },
+          { text: 'Local', link: '/advanced/local' },
           { text: 'Extensibility',  collapsed : "false" , items: [
             { text: 'Custom JS', link: '/advanced/extensibility/custom_js' },
             { text: 'Frontend', link: '/advanced/extensibility/frontend' },
---
+++ docs/advanced/local.md
@@ -0,0 +1,6 @@
+# Local 
+
+abap2UI5-local is a special build of abap2UI5 which combines all artifacts into a single http handler. You just need to create an additional database table and can run abap2UI% independed form the rest of the system or any other abap2UI5 installation.
+
+Find all information in the repository: <br>
+https://github.com/abap2UI5/abap2UI5-local
