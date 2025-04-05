2a17c78cf7c05d9075cddfdbeabdd6d0e58f4a19
oblomov-dev <102328295+oblomov-dev@users.noreply.github.com>
2025-04-05 10:17:50 +0200

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -57,7 +57,7 @@ export default defineConfig({
         ]
       },
       {
-        text: '1.137.0', items: [
+        text: '1.138.0', items: [
           { text: 'Changelog', link: '/resources/changelog' },          
           { text: 'Support', link: '/resources/support' },
           { text: 'Contribution', link: '/resources/contribution' },
